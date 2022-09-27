0d450c41c7781044c76c9300e27980b4ddf51254
lkk223303 <lkk223303@gmail.com>
2022-09-27 23:53:47 +0800

    chat room

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+type Message struct {
+	Event   string `json:"event"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+func NewMessage(event, name, content string) *Message {
+	return &Message{
+		Event:   event,
+		Name:    name,
+		Content: content,
+	}
+}
+
+func (m *Message) GetByteMessage() []byte {
+	result, _ := json.Marshal(m)
+	return result
+}
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("template/html/*")
+	r.Static("/assets", "./template/assets")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	m := melody.New()
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	m.HandleConnect(func(session *melody.Session) {
+		id := session.Request.URL.Query().Get("id")
+		m.Broadcast(NewMessage("other", id, "加入聊天室").GetByteMessage())
+	})
+
+	m.HandleClose(func(session *melody.Session, i int, s string) error {
+		id := session.Request.URL.Query().Get("id")
+		m.Broadcast(NewMessage("other", id, "離開聊天室").GetByteMessage())
+		return nil
+	})
+	r.Run(":8080")
+}
